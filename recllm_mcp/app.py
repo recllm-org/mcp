2360e0f3e1b6c01a2fca9d38cc5a2cdbcbe5c51b
Pranav Sastry <pranava.sri@gmail.com>
2025-04-29 00:25:03 +0530

    fixing indentation and import errors

--- recllm_mcp/app.py
+++ recllm_mcp/app.py
@@ -5,10 +5,10 @@ from .db import BasicDatabase
 
 class RecLLMApp(FastAPI):
   def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		self.db = BasicDatabase()
-	
-	def add_routers(self, routers):
-		routers = [getattr(routers, attr) for attr in dir(routers) if attr.endswith('router')]
-		for router in routers:
-			self.include_router(router)
+    super().__init__(*args, **kwargs)
+    self.db = BasicDatabase()
+  
+  def add_routers(self, routers):
+    routers = [getattr(routers, attr) for attr in dir(routers) if attr.endswith('router')]
+    for router in routers:
+      self.include_router(router)
--- recllm_mcp/dependencies.py
+++ recllm_mcp/dependencies.py
@@ -3,7 +3,7 @@ from fastapi import Request
 
 
 def get_db(request: Request):
-	try:
-		yield request.app.db
-	finally:
-		pass  # Connection handling is managed by BasicDatabase class
+  try:
+    yield request.app.db
+  finally:
+    pass  # Connection handling is managed by BasicDatabase class

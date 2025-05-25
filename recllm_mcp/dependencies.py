f28c8a71267c5091a8985f0ac13a418f55a341d2
Pranav Sastry <pranava.sri@gmail.com>
2025-05-25 13:19:00 +0530

    reorganized dependencies to be in line with recllm_api

--- recllm_mcp/dependencies.py
+++ recllm_mcp/dependencies.py
@@ -11,7 +11,6 @@ def get_db(request: Request):
 	finally:
 		pass  # Connection handling is managed by BasicDatabase class
 
-
 def get_user(api_key, db):
   existing_tables = db.pull_existing_tables(['user'])
   UserTable = existing_tables['user']
@@ -19,33 +18,40 @@ def get_user(api_key, db):
     user = session.query(UserTable).filter(UserTable.api_key==api_key).first()
   return user
 
+def get_api_key(request: Request):
+  # Authorization: Bearer 
+  api_key = request.headers.get('authorization')
+  if api_key:
+    api_key = api_key.replace('Bearer ', '')
+  # RecLLM-API-Key
+  if not api_key:
+    api_key = request.headers.get('RecLLM-API-Key')
+  return api_key
+
+def validate_api_key(api_key):
+  try:
+    UUID(api_key)
+  except ValueError:
+    return False
+  return True
+
 
 class AuthMiddleware(BaseHTTPMiddleware):
   async def dispatch(self, request: Request, call_next):
-    request.state.db = next(get_db(request))
-    # Get API Key
-    api_key = request.headers.get('authorization')
-    # Authorization: Bearer 
-    if api_key:
-      api_key = api_key.replace('Bearer ', '')
-    # RecLLM-API-Key
-    if not api_key:
-      api_key = request.headers.get('RecLLM-API-Key')
+    api_key = get_api_key(request)
     if not api_key:
       return JSONResponse(
         status_code=status.HTTP_401_UNAUTHORIZED,
         content={'detail': 'API key is missing'}
       )
     # Validate if api_key is a valid UUID
-    try:
-      UUID(api_key)
-    except ValueError:
+    if not validate_api_key(api_key):
       return JSONResponse(
         status_code=status.HTTP_401_UNAUTHORIZED,
         content={'detail': 'Invalid API key format - must be a valid API Key'}
       )
     # Get user
-    user = get_user(api_key, request.state.db)
+    user = get_user(api_key, request.app.db)
     if not user:
       return JSONResponse(
         status_code=status.HTTP_401_UNAUTHORIZED,
